147036d212e1499f4e1fe6cc48618535330ff2dc
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-04-07 08:47:22 -0400

    Add: top-k-elements-in-list - submission-55

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-55.py
@@ -0,0 +1,164 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #Attempt 
+        bucket = [[] for _ in range(len(nums)+1)]
+        res = []
+        dic = {}
+        for num in nums:
+            dic[num] = dic.get(num, 0) + 1
+        for num,freq in dic.items():
+            bucket[freq].append(num)
+        for i in range(len(bucket)-1,-1,-1):
+            if k==0:
+                return res
+            for num in bucket[i]:
+                res.append(num)
+                k-=1
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''#Attempt 5
+        bucket=[[] for _ in range(len(nums) + 1)]
+        dic={}   
+        res=[]
+        if len(nums)==k:
+            return nums
+        for idx,num in enumerate(nums):
+            dic[num]=1+dic.get(num,0)
+        for key,value in dic.items():
+            bucket[value].append(key)
+        print(bucket)
+        for i in range(len(bucket)-1,0,-1):
+            for j in bucket[i]:
+                if len(res)<k:
+                    res+=[j]
+        print(dic)
+        return res'''
+        
+
+
+            
+
+
+
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Attempt 4
+        '''import heapq
+        dic={}
+        lst=[]
+        for num in nums:
+            dic[num]=1+dic.get(num,0)
+        res=[]
+        for num,freq in dic.items():
+            lst+=[freq]
+        print(dic,'dic')
+
+        lst=[-n for n in lst]
+        print(lst,'lst')
+        heapq.heapify(lst)
+        for _ in range(k):
+            res+=[dic[-heapq.heappop(lst)]]
+        print(res)'''
+
+
+
+
+
+
+
+        #Attempt #3
+        '''freq={}
+        res=[]
+        bucket=[[] for i in range(len(nums)+1)]
+        for num in nums:
+            freq[num]=1+freq.get(num,0)
+        for key,value in freq.items():
+            bucket[value]+=[key]
+        for i in range(len(bucket)-1,0,-1):
+            for j in bucket[i]:
+                if len(res)<k:
+                    print(j)
+                    res+=[j]
+        return res'''
+        #Attempt 2
+        '''val=[]
+        from collections import Counter
+        lst=Counter(nums)
+        lst = lst.most_common()
+        print(lst,'base')
+        while len(lst)>k:
+            lst.pop()
+        print(lst)
+        for num in lst:
+            val+=[num[0]]
+        return val'''
+        #Attempt 1
+        '''if len(nums)==k:
+            return nums
+        dic={}
+        lst=[]
+        for num in nums:
+            if num in dic:
+                dic[num]+=1
+            else:
+                dic[num]=1
+        return lst'''
+
+
+
+        
