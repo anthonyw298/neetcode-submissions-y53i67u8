0e3b85e3e9c63180cae7e635f048610547106036
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-04-07 10:03:34 -0400

    Add: valid-binary-search-tree - submission-8

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-8.py
@@ -0,0 +1,118 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        #Attempt 4
+
+        def valid(root,Min,Max):
+            if not root:
+                return True
+            if root.val>=Max or root.val<=Min:
+                return False
+            return valid(root.left,Min,root.val) and valid(root.right,root.val,Max)
+            
+
+        return valid(root,float('-inf'),float('inf'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''#Attempt 3
+
+        def range(root,small,large):
+            if not root:
+                return True
+            if root.val>=large or root.val<=small:
+                return False
+            return range(root.left,small,root.val) and range(root.right,root.val,large)
+        return range(root,float('-infinity'),float('infinity')) '''
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Attempt 2
+        '''return self.check(root,float('-inf'),float('inf'))
+    def check(self,root,low,high):
+        if not root:
+            return True
+        if not (low<root.val<high):
+            return False
+        return self.check(root.left,low,root.val) and self.check(root.right,root.val,high)'''
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Attempt 1
+        '''if not root:
+            return False 
+        while root.left or root.right:
+            if not root.left and not root.right:
+                return True
+            elif not root.right:
+                if root.left.val<root.val:
+                    return self.isValidBST(root.left)
+                else:
+                    return False
+            elif not root.left:
+                if root.right.val>root.val:
+                    return self.isValidBST(root.right)
+                else:
+                    return False
+            elif root.left.val<root.val and root.right.val>root.val:
+                return self.isValidBST(root.left) and self.isValidBST(root.right)
+            else:
+                return False
+        return True'''
+        
