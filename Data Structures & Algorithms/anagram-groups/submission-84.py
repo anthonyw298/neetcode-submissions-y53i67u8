bcd4fdb536132721fbd24757b45f33f1281bf58e
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-04-08 08:15:01 -0400

    Add: anagram-groups - submission-84

---
+++ Data Structures & Algorithms/anagram-groups/submission-84.py
@@ -0,0 +1,154 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #Attempt 4
+        res=[]
+        anagrams=defaultdict(list)
+        for string in strs:
+            count = [0] * 26
+            for char in string:
+                count[ord(char) - ord('a')] += 1
+            anagrams[tuple(count)].append(string)
+        for key,value in anagrams.items():
+            res.append(value)
+        return res
+
+            
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Attempt 3
+        '''dic={}
+        for string in strs:
+            sig=[0]*26
+            for char in string:
+                sig[ord(char)-ord('a')]+=1
+            sig=tuple(sig)
+            if sig in dic:
+                dic[sig]+=[string]
+            else:
+                dic[sig]=[string]
+        print(dic)
+        return list(dic.values())'''
+            
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Attempt 2
+        '''dic={}
+        if len(strs)<2:
+            return [strs]
+
+        for string in strs:
+            signature=[0]*26
+            for char in string:
+                signature[ord(char)-ord('a')]+=1
+            signature = tuple(signature)
+            if signature not in dic:
+                    dic[signature]=[string]
+            else:
+                    dic[signature]+=[string]
+        return list(dic.values())'''
+        
+        #Attempt 1
+        '''if len(strs)<2:
+            return [strs]
+        dic={}
+        for i,string in enumerate(strs):
+            count=[0]*26
+            for j,char in enumerate(string):
+                count[ord(char)-ord('a')]+=1
+            print(count)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
