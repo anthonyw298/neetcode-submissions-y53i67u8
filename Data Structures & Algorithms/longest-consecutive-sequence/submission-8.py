16398856e4e6a150c810655a7ada780bf2e10823
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-04-21 07:27:40 -0400

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.py
@@ -0,0 +1,123 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        seen = set(nums)
+        high = 0
+        for i in range(len(nums)):
+            local = 0
+            if nums[i] - 1 not in nums:
+                while nums[i] + local in seen:
+                    local += 1
+                high = max(high, local)
+        return high
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''#Attempt 3
+        print(nums)
+        print(set(nums))
+        res=0
+        cnt=0
+        for idx,num in enumerate(nums):
+            if idx!=0:
+                if num-nums[idx-1]==0:
+                    continue
+                elif num-nums[idx-1]==1:
+                    print('count incremented')
+                    cnt+=1
+                else:
+                    print('checked')
+                    if cnt>res:
+                        res=cnt
+                    cnt=1
+            else:
+                cnt+=1
+        if cnt>res:
+            res=cnt
+        return res'''
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Attempt 2
+        '''if len(nums)<2:
+            return len(nums)
+        cnt=1
+        res=0
+        nums=list(sorted(set(sorted(nums))))
+        print(nums,'nums')
+        for i in range(1,len(nums),1):
+            if nums[i]-nums[i-1]==1:
+                cnt+=1
+            else:
+                if res<cnt:
+                    res=cnt
+                cnt=1
+        if res<cnt:
+            res=cnt
+        return res'''
