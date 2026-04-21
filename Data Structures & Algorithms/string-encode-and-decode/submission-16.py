1ba07031a769612051cfd921cc0e594d6cef59e5
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-04-21 05:17:40 -0400

    Add: string-encode-and-decode - submission-16

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-16.py
@@ -0,0 +1,196 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        res = []
+        for word in strs:
+            res.append(str(len(word)) + '#' + word)
+        return "".join(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''#Attempt 2
+        lst=[]
+        for string in strs:
+            lst+=[str(len(string)),'#']
+            for char in string:
+                lst+=[char]
+        return "".join(lst)'''
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Attempt 1
+        '''encode=''
+        for word in strs:
+            print(word,'word')
+            for char in word:
+                print(char,'char')
+                encode+=str(ord(char))
+                encode+=" "
+                print(encode,'encode')
+            encode+=','
+        print(encode)
+        return encode'''
+    def decode(self, s: str) -> List[str]:
+        print(s)
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            res.append(s[i:j])
+            i =j
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''#Attempt 2
+        print(s,len(s))
+        r=0
+        l=0
+        res=[]
+        while r<len(s):
+            while s[r]!="#":
+                r+=1
+            length=int(s[l:r])
+            print(length,r,l)
+            res+=[s[r+1:r+length+1]]
+            r+=length+1
+            print(r)
+            l=r
+        return res'''
+
+            
+
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Attempt 1
+        '''print(s,'decode stage')
+        decode=[]
+        letter=''
+        word=''
+        for char in s:
+            if char==",":
+                decode+=[word]
+                word=""
+            elif char ==" ":
+                word+=str(chr(int(letter)))
+                print(word)
+                letter=""
+            else:       
+                letter+=str(char)
+        return decode'''
