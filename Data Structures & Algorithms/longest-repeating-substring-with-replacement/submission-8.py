1bfc2b15496b69afa7ace913a7230007ef404f92
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-04-21 10:39:06 -0400

    Add: longest-repeating-substring-with-replacement - submission-8

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-8.py
@@ -0,0 +1,197 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        seen = {}
+        most = 0
+        maxFreq = 0
+        l = 0
+        for r in range(len(s)):
+            seen[s[r]] = seen.get(s[r],0) + 1
+            maxFreq = max(maxFreq,seen[s[r]])
+            while maxFreq + k < (r - l + 1):
+                print(r)
+                seen[s[l]] = seen.get(s[l], 0) - 1
+                l += 1
+            most = max(most, (r - l + 1))
+        return most
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''#sliding window
+        l = 0
+        res = 0
+        count = {}
+        maxFreq = 0
+        for r in range(len(s)):
+            count[s[r]] = count.get(s[r],0) + 1
+            maxFreq = max(maxFreq,count[s[r]])
+            while (r - l + 1) - maxFreq > k:
+                count[s[l]] = count.get(s[l], 0) - 1
+                l += 1
+            res = max(res, r - l + 1)
+        return res'''
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #Attempt 2- Brute Force? -sum
+        '''l,res,dic=0,0,{}
+        if k>=len(s):
+            return len(s)
+        for r in range(len(s)):
+            dic[s[r]]=1+dic.get(s[r],0)
+            while sum(dic.values())-max(dic.values())>k:
+                res=max(res,sum(dic.values())-1)
+                dic[s[l]]-=1
+                l+=1
+                print(dic,'1')
+            print(dic,'2')
+        print(dic,'3')
+        return max(res,sum(dic.values()))'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #Attempt 1
+        '''l , r, res = 0,0,0
+        dic={}
+        if r==0:
+            dic[s[r]]=1+dic.get(s[r],0)
+            r+=1
+        print(dic)
+        while r<len(s):
+            while max(dic.values())+k>=sum(dic.values()):
+                dic[s[r]]=1+dic.get(s[r],0)
+                r+=1
+                if r==len(s):
+                    break
+                print(dic)
+            if res<max(dic.values())+ min(sum(dic.values()) - max(dic.values()), k): 
+                res=max(dic.values())+ min(sum(dic.values()) - max(dic.values()), k)
+                print(res)
+            while max(dic.values())+k<sum(dic.values()):
+                dic[s[l]]=dic.get(s[l],0)-1
+                l+=1
+                print(dic)
+            print(max(dic.values()))
+        if res<max(dic.values())+ min(sum(dic.values()) - max(dic.values()), k): 
+                res=max(dic.values())+ min(sum(dic.values()) - max(dic.values()), k)
+                print(res)
+        print(dic)
+        return res'''
+
+
+            
+
+
+        
