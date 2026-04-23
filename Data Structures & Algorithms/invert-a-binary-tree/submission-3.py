f62b80ccc79c28cc9d6ea846cc36d5699d28469e
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-04-23 09:20:58 -0400

    Add: invert-a-binary-tree - submission-3

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-3.py
@@ -0,0 +1,113 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        left = self.invertTree(root.left)
+        right = self.invertTree(root.right)
+        root.left , root.right = root.right , root.left
+        return root
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        '''#Attempt 4
+            if not root:
+                return 
+            root.left = self.invertTree(root.left)
+            root.right = self.invertTree(root.right)
+            root.right,root.left = root.left,root.right
+            return root'''
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            #Attempt 3-Stack
+        '''if not root:
+                return
+            stack=[root]
+            while stack:
+                node=stack.pop()
+                if node.left:
+                    stack.append(node.left)
+                if node.right:
+                    stack.append(node.right)
+                node.left,node.right=node.right,node.left
+            return root'''
+
+
+            #Attempt 2-BFS
+        '''from collections import deque
+            if not root:
+                return
+            queue=([root])
+            while queue:
+                node=queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)'''
+
+
+            #Attempt 1-DFS
+        '''if not root:
+                return
+            else:
+                rootL=self.invertTree(root.left)
+                rootR=self.invertTree(root.right)
+                root.left,root.right=rootR,rootL
+                return root'''
