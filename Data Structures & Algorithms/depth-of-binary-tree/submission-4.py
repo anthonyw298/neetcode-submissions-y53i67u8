143055b896e944bda546b80a0707261304e10f26
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-04-23 09:25:08 -0400

    Add: depth-of-binary-tree - submission-4

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-4.py
@@ -0,0 +1,119 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left),self.maxDepth(root.right)) + 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''#Attempt 4
+        if not root:
+            return 0
+        cnt=1
+        return max(self.dfs(root.left,cnt),self.dfs(root.right,cnt))
+    def dfs(self,root,cnt):
+        if not root:
+            return cnt
+        cnt+=1
+        return max(self.dfs(root.left,cnt),self.dfs(root.right,cnt))'''
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Attempt 3 - Stack
+        '''if not root:
+            return 0
+        stack=[(root,1)]
+        res=0
+        while stack:
+            node,val=stack.pop()
+            res=max(res,val)
+            if node.left:
+                stack.append((node.left,val+1))
+            if node.right:
+                stack.append((node.right,val+1))
+        return res'''
+        #Attempt 2 - BFS
+        '''from collections import deque
+        if not root:
+            return 0
+
+        queue=deque([(root,1)])
+        res=0
+        while queue:
+            node,val=queue.popleft()
+            res=max(res,val)
+            if node.left:
+                queue.append((node.left,val+1))
+            if node.right:
+                queue.append((node.right,val+1))
+        return res
+            '''
+
+
+        #Attempt 1-DFS
+        '''if not root:
+            return 0
+        return max(self.maxDepth(root.left)+1,self.maxDepth(root.right)+1)'''
+        
+        
