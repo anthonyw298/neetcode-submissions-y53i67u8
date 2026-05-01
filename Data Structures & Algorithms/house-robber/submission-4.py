99c2589548ec58f1b3609afdae85baeca355c756
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-05-01 07:02:32 -0400

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,121 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev1, prev2 = 0, 0
+        for i in range(2,len(nums) + 2):
+            tmp = prev1
+            prev1 = max(prev1, prev2 + nums[i-2])
+            prev2 = tmp
+        return prev1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''#Attempt 3
+        cache = {}
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+            if i in cache:
+                return cache[i]
+            cache[i] = max(nums[i] + dfs(i + 2), dfs(i + 1))
+            return cache[i]
+        
+        return dfs(0)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''#Attempt 2
+        rob1,rob2 = 0,0
+        for num in nums:
+            temp = rob1
+            rob1=rob2
+            rob2=max(temp+num,rob2)
+        return rob2'''
+        #Attempt 1
+        '''sum1=sum2=0
+        for i in range(len(nums)):
+            if i%2==0:
+                sum1+=nums[i]
+            else:
+                sum2+=nums[i]
+
+        print(sum1,sum2)
+        return max(sum1,sum2)'''
