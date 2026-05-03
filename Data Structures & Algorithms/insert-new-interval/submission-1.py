bafe433c92aee166f3cf47a47236c1ec86bb8783
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-05-03 14:02:40 -0400

    Add: insert-new-interval - submission-1

---
+++ Data Structures & Algorithms/insert-new-interval/submission-1.py
@@ -0,0 +1,87 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        newStart, newEnd = newInterval[0], newInterval[1]
+        res = []
+        for start, end in intervals:
+            if end < newStart:
+                res.append([start, end])
+            elif start > newEnd:
+                res.append([newStart, newEnd])
+                newStart, newEnd = start, end
+            else:
+                newStart, newEnd = min(newStart, start), max(newEnd, end)
+        res.append([newStart, newEnd])
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    '''#Attempt 2
+        res=[]
+        for i in range(len(intervals)):
+            start=intervals[i][0]
+            end=intervals[i][1]
+            if start>newInterval[1]:
+                res.append(newInterval)
+                return res+intervals[i:]
+            elif end<newInterval[0]:
+                res.append(intervals[i])
+            else:
+                newInterval=[min(start,newInterval[0]),max(end,newInterval[1])]
+        res.append(newInterval)
+        return res'''
+            
+        #Attempt 1
+    '''res=[]
+        newStart=newInterval[0]
+        newEnd=newInterval[1]
+        for start,end in intervals:
+            if newStart>end or newEnd < start:
+                res.append([start,end])
+            else:'''
+                
+                
+
+
+        
