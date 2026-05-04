4a226b6112fb40e64ec0b11831191f911059f74a
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-05-04 11:06:03 -0400

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.py
@@ -0,0 +1,68 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        for i in range(len(dp)):
+            dp[i] = dp[i >> 1] + (i & 1)
+        return dp
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''#4//2=2r0//2=1r0//2=0r1->100
+        #nested while loop 1) for each n from 0 to n 2) to convert to bianry and count 1s
+res=[]
+        for num in range(n+1):
+            one=0
+            for i in range(32):
+                if num & (1<<i):
+                    one+=1
+            res.append(one)
+        return res'''
+'''res = [0] * (n+1)
+        offset=1
+        
+        for num in range(1,n+1):
+            if offset*2==num:
+                offset=num
+            res[num]=(res[num-offset]+1)
+        return res'''
+
+
