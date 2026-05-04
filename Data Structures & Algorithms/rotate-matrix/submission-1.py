b5d6262b0e89635fbb1ab9fd49a6ef909c819a1b
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-05-04 05:57:03 -0400

    Add: rotate-matrix - submission-1

---
+++ Data Structures & Algorithms/rotate-matrix/submission-1.py
@@ -0,0 +1,68 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        rows, cols = len(matrix), len(matrix[0])
+        for i in range(rows//2):
+            matrix[i], matrix[rows - i - 1] = matrix[rows - i - 1], matrix[i]
+        for i in range(rows):
+            for j in range(i,cols):
+                matrix[i][j] , matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''#not sure how to reverse all diagonals
+        for i in range(len(matrix)):
+            for j in range(i+1,len(matrix[i])):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+        for row in matrix:
+            row.reverse()'''
+
+        
+
+
+
+
+        
