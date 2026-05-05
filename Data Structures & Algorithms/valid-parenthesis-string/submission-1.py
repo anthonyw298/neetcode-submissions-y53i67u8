c29eebd9bdf2cc77f1a2aafc0f586f6f13af901d
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-05-05 10:53:17 -0400

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.py
@@ -0,0 +1,85 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        low = high = 0
+        for char in s:
+            if char == '(':
+                low += 1
+                high += 1
+            elif char == ')':
+                low -= 1
+                high -= 1
+            else:
+                low -= 1
+                high += 1
+            if low < 0:
+                    low = 0
+            if high < 0:
+                    return False
+        return low == 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''low = 0   # min possible unmatched '('
+        high = 0  # max possible unmatched '('
+
+        for char in s:
+            if char == '(':
+                low += 1
+                high += 1
+            elif char == ')':
+                low -= 1
+                high -= 1
+            else:  # '*'
+                low -= 1   # treat '*' as ')'
+                high += 1  # treat '*' as '('
+        
+            if high < 0:      # too many ')' in best case
+                return False
+            low = max(low, 0) # can't go below 0
+
+        return low == 0'''
+
+        
