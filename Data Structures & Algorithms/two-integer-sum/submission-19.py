5388b57edd8a9eb3d7ea55ca22b1ac5d4cd9bbeb
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-05-08 17:01:01 -0400

    Add: two-integer-sum - submission-19

---
+++ Data Structures & Algorithms/two-integer-sum/submission-19.py
@@ -0,0 +1,139 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # {} key - value we visited value: index
+        visit = {}
+        for i in range(len(nums)):
+            if target - nums[i] in visit:
+                return [visit[target - nums[i]], i]
+            else:
+                visit[nums[i]] = i
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''dic = {}
+        for i in range(len(nums)):
+            num = nums[i]
+            complement = target - num
+            if complement in dic:
+                return [dic[complement],i]
+            dic[num] = i'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''
+        #Attempt 2
+
+        l , r = 0,len(nums)-1
+        while l < r:
+            if nums[l]+nums[r]>target:
+                r-=1
+            elif nums[l]+nums[r]<target:
+                l+=1
+            else:
+                return [l,r]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Attempt 1
+        idx=[]
+        for i in range(len(nums)):
+            if target-nums[i] in nums[i+1:]:
+                idx+=[i]
+            else:
+                if len(idx)>0:
+                    if nums[i]==target-nums[idx[0]]:
+                        idx+=[i]
+                        return idx'''
+
+    
