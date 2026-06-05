e443aadd045ba906e982e0c58d9fcdf61ef8eefb
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-06-05 12:27:24 -0400

    Add: two-integer-sum - submission-22

---
+++ Data Structures & Algorithms/two-integer-sum/submission-22.py
@@ -0,0 +1,183 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+            if target - nums[i] in dic:
+                return [dic[target - nums[i]], i]
+            else:
+                dic[nums[i]] = i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''
+        # {} key - value we visited value: index
+        visit = {}
+        for i in range(len(nums)):
+            if target - nums[i] in visit:
+                return [visit[target - nums[i]], i]
+            else:
+                visit[nums[i]] = i
+        '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''dic = {}
+        for i in range(len(nums)):
+            num = nums[i]
+            complement = target - num
+            if complement in dic:
+                return [dic[complement],i]
+            dic[num] = i'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''
+        #Attempt 2
+
+        l , r = 0,len(nums)-1
+        while l < r:
+            if nums[l]+nums[r]>target:
+                r-=1
+            elif nums[l]+nums[r]<target:
+                l+=1
+            else:
+                return [l,r]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Attempt 1
+        idx=[]
+        for i in range(len(nums)):
+            if target-nums[i] in nums[i+1:]:
+                idx+=[i]
+            else:
+                if len(idx)>0:
+                    if nums[i]==target-nums[idx[0]]:
+                        idx+=[i]
+                        return idx'''
+
+    
