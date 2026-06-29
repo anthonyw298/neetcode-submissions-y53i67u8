28288f3972e8eb37f8f483c9f180156976d2fea4
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2026-06-29 08:25:26 -0400

    Add: interleaving-string - submission-13

---
+++ Data Structures & Algorithms/interleaving-string/submission-13.py
@@ -0,0 +1,124 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+        dp = {}
+        def dfs(i, j):
+            if i >= len(s1) and j >= len(s2):
+                return True
+            elif (i, j) in dp:
+                return dp[(i,j)]
+            if (i < len(s1) and s1[i] == s3[i + j]) and (j < len(s2) and s2[j] == s3[i + j]):
+                dp[(i, j)] = dfs(i + 1, j) or dfs(i, j + 1)
+            elif i < len(s1) and s1[i] == s3[i + j]:
+                dp[(i,j)] = dfs(i + 1, j)
+            elif j < len(s2) and s2[j] == s3[i + j]:
+                dp[(i,j)] = dfs(i, j + 1)
+            else:
+                dp[(i,j)] = False
+            return dp[(i, j)]
+        return dfs(0,0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''
+        if len(s1) + len(s2) != len(s3):
+            return False
+        dp = {}
+        def dfs(i,j):
+            if i >= len(s1) and j >= len(s2):
+                return True
+            if (i,j) in dp:
+                return dp[(i,j)]
+            dp[(i,j)] = (i < len(s1) and dfs(i + 1,j) and s3[i + j] == s1[i] ) or (j < len(s2) and dfs(i,j + 1) and s3[i + j] == s2[j])
+
+            return dp[(i,j)]
+        return dfs(0,0)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''
+        if len(s1) + len(s2) != len(s3):
+            return False
+        
+        dp = [[False] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+        dp[0][0] = True
+        
+        for i in range(len(s1) + 1):
+            for j in range(len(s2) + 1):
+                if i > 0:
+                    dp[i][j] |= dp[i-1][j] and s1[i-1] == s3[i+j-1]
+                if j > 0:
+                    dp[i][j] |= dp[i][j-1] and s2[j-1] == s3[i+j-1]
+        
+        return dp[len(s1)][len(s2)]'''
+
+        
+        
